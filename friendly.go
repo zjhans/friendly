84db954a602e1fbb0be3c808c2564e6978852498
zjhans <55612469+zjhans@users.noreply.github.com>
2019-09-25 09:38:47 -0400

    Create friendly.go

---
+++ friendly.go
@@ -0,0 +1,45 @@
+package main
+  
+import (
+        "fmt"
+        "net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+        html := `<html>
+                <title>Who are you?</title>
+                <body>
+
+                <form action="http://34.229.188.206:8080/getname" method="post" enctype="multipart/form-data">
+                <label for="text">Please enter your name:</label>
+                <input type="text" name="name">
+                <input type="submit" name="submit" value="Submit">
+                </form>
+
+                </body>
+                </html>`
+
+                w.Write([]byte(fmt.Sprintf(html)))
+        }
+
+func GetName(w http.ResponseWriter, r *http.Request) {
+        err := r.ParseMultipartForm(200000) //Grab and error check text form and send err to browser
+
+        if err != nil {
+                fmt.Fprintln(w, err)
+                return
+        }
+
+        //write out a response with the provided name to both console and browser
+        fmt.Fprintln(w, "Hello", r.FormValue("name"), "and thank you very much for having me here today!")
+        fmt.Println("We just heard from:", r.FormValue("name"))
+}
+
+func main() {
+        mux := http.NewServeMux()
+
+        mux.HandleFunc("/", Home)
+        mux.HandleFunc("/getname", GetName)
+
+        http.ListenAndServe(":8080", mux)
+}
